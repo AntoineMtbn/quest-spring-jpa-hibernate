8b7798a202635fbba667781da559a6e2e19cba82
AntoineMtbn <antoine.montauban@gmail.com>
2023-06-28 12:20:36 +0200

    Quete Hibernate 1

--- database/spring_jpa_quest.sql
+++ database/spring_jpa_quest.sql
@@ -2,3 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `spring_jpa_quest` /*!40100 DEFAULT CHARACTER SET
 USE `spring_jpa_quest`;
 DROP TABLE IF EXISTS `school`;
 DROP TABLE IF EXISTS `wizard`;
+
+CREATE USER 'h4rryp0tt3r'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Horcrux4life!';
+GRANT ALL PRIVILEGES ON `spring_jpa_quest`.* TO 'h4rryp0tt3r'@'localhost';
+FLUSH PRIVILEGES;
